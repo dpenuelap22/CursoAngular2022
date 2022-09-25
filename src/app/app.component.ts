ee4bf1de63b32c0a66b392f6b47691373d9aea2e
depenuelap22 <dpenuelap@unal.edu.co>
2022-09-25 16:10:01 -0500

    Version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { AST } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+
+
+interface Tarjeta{
+  titulo:string;
+  subtitulo:string;
+  nro?:number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'TAREAAPP';
+export class AppComponent implements OnInit{
+
+  title = 'Mi primer App ANGULAR';
+  public ArregloTarjetas:| Tarjeta[]=[]
+
+  ngOnInit(): void {
+
+    this.ArregloTarjetas=[
+  {titulo:'video 1', subtitulo:'subtitulo video 1'},
+  {titulo:'video 2', subtitulo:'subtitulo video 2'},
+  {titulo:'video 3', subtitulo:'subtitulo video 3'}
+
+
+    ]
+  }
+
 }
---
+++ src/app/card/card.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+  public image:string="https://lh6.googleusercontent.com/0wgpMYzA0Aa2dBQSlk6-53FPqgjYkwSUvIpL1PsYZOKF3f4XgkqGQPLFrzIvg1fzqVf81luTLlIX5zS9ytmzD6WDWfieXuAIZLmnkkAQ_kFKc3p-cvTEcvwwYX7vp9HJwk2hP3v1";
+  public titulo:string="El cuerpo Humano es increible";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
